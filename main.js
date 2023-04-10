b9e9570d5e5cf9058a4c47c18ea10f1c0d55ea3d
Matthew Zenn <spyguy57@gmail.com>
2023-04-10 19:26:55 -0400

    added clear fucntion

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const code = document.getElementById("code");
 
 code.addEventListener("keypress", function onEvent(event) {
     if (event.key === "Enter") {
-        eval(code.value)
+        eval(code.value);
+        code.value = "";
     }
 });
