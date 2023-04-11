60a42809d0933b3b7697d6f9cc5588e9ee6e020b
Matthew Zenn <spyguy57@gmail.com>
2023-04-10 20:16:05 -0400

    bugs

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const code = document.getElementById("code");
 code.addEventListener("keypress", function onEvent(event) {
     if (event.key === "Enter") {
         eval(code.value);
-        code.value = "";
+        code.value = ""; 
     }
 });
